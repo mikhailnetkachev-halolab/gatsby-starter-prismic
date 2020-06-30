46085b51843345628de2bec490db58882939c8f0
mikhailnetkachev-halolab <michael.n@halo-lab.com>
2020-06-30 09:32:33 +0300

    Edited global styles: renamed global classes.

--- src/components/templates/Layout/components/Footer/Footer.js
+++ src/components/templates/Layout/components/Footer/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import cs from 'classnames';
 
 import styles from './styles.module.scss';
 
 const Footer = () => {
-  return <div className={cs(styles.container, 'container')}>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className="wrapper">Footer</div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/templates/Layout/components/Header/Header.js
+++ src/components/templates/Layout/components/Header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import cs from 'classnames';
 
 import styles from './styles.module.scss';
 
 const Header = () => {
-  return <div className={cs(styles.container, 'container')}>Header</div>;
+  return (
+    <div className={styles.container}>
+      <div className="wrapper">Header</div>
+    </div>
+  );
 };
 
 export default Header;
